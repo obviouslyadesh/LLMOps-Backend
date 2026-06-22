f966999c51c6d54c9580d1532a1633c2e419ec69
obviouslyadesh <adeshbohara333@gmail.com>
2026-06-22 18:07:08 +0545

    Added qdrant api

--- app/services/qdrant_service.py
+++ app/services/qdrant_service.py
@@ -1,8 +1,5 @@
 from qdrant_client import QdrantClient
-from qdrant_client.models import (
-    Distance,
-    VectorParams,
-)
+from qdrant_client.models import Distance, VectorParams
 
 from app.core.config import settings
 
@@ -11,29 +8,36 @@ class QdrantService:
     COLLECTION_NAME = "documents"
 
     def __init__(self):
-
-        self.client = QdrantClient(url=settings.QDRANT_URL)
+        self.client = QdrantClient(
+            url=settings.QDRANT_URL,
+            api_key=settings.QDRANT_API_KEY or None,
+        )
 
         self._create_collection()
 
     def _create_collection(self):
-
         collections = self.client.get_collections()
-
-        names = [collection.name for collection in collections.collections]
+        names = [c.name for c in collections.collections]
 
         if self.COLLECTION_NAME not in names:
             self.client.create_collection(
                 collection_name=self.COLLECTION_NAME,
-                vectors_config=VectorParams(size=384, distance=Distance.COSINE),
+                vectors_config=VectorParams(
+                    size=384,
+                    distance=Distance.COSINE,
+                ),
             )
 
     def insert_chunks(self, points):
-        self.client.upsert(collection_name=self.COLLECTION_NAME, points=points)
+        self.client.upsert(
+            collection_name=self.COLLECTION_NAME,
+            points=points,
+        )
 
     def search(self, vector, limit=5):
         results = self.client.query_points(
-            collection_name=self.COLLECTION_NAME, query=vector, limit=limit
+            collection_name=self.COLLECTION_NAME,
+            query=vector,
+            limit=limit,
         )
-
         return results.points
